576296efb0d15e5dc8efcf1e07e5c5a7ae29f380
CY <cywhiz@gmail.com>
2017-05-05 17:56:41 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # KulrSpottr
+
+## Description
+KulrSpottr is a game where the player has to spot the tile which has a different colour in a matrix of coloured tiles. If the player clicks the right tile, the game continues and the player moves on to the next level of difficulty. The game ends when the player chooses the wrong tile.
+
+## Prerequisities
+Any modern Web browser with JavaScript enabled. Used crossbrowsertesting.com and also real devices to test cross-platform compatibilities. Fully compatible with the following popular Web browsers:
+* Windows: (Chrome, Firefox, IE Edge)
+* Linux: Chrome, Firefox
+* Android: Chrome, Firefox
+* iOS (iPhone): Chrome, Safari
+
+## Usage
+1. Clone the repository to a local folder by running `git clone https://github.com/cywhiz/Kulrspottr.git`
+2. Swtich to the project folder
+3. Click on `index.html` to run the game
+
+A live demo of the app can be accessed through https://kulrspottr.herokuapp.com
+
+## Tools/Frameworks Used
+This app was created with a goal to make it as simple as possible, without using unnecessary frameworks to clutter the app. It uses mostly pure JavaScript, CSS, and HTML. Only jQuery and Bootstrap are used to simplify tasks.
+
+### jQuery 3
+jQuery was used for its ability to quickly and easily edit the DOM.
+
+### Bootstrap 3
+Bootstrap is a popular framework used to make the game work and look similar on all common browsers and systems, including mobile devices. The framework provides responsiveness across all devices and has auto spacing and indentation adjustment.
+
+## Improvements/bugs
+- Error handling and validation in player name input, specifying a min/max length
+- More animations and GUI/font improvements
+- Add timing in additional to score
+- Game is currently not optimized for mobile phones in landscape mode
+- Add game options like difficulty level (colour contrast)
+- High scores info is lost when page is refreshed. Perhaps `sessionStorage` or `localStorage` can be used to keep the scores longer
---
+++ js/game.js
@@ -0,0 +1,149 @@
+var game;
+var scores = [];
+
+class KulrSpottr {
+    constructor() {
+        // Initialize the game
+        game = this;
+        game.newGame();
+
+        // Register and monitor click events
+        $(document).on('click', '.tile', game.checkTile);
+        $(document).on('click', '#showHint', game.showHint);
+        $(document).on('click', '#addScore', game.addScore);
+        $(document).on('click', '#newGame', game.newGame);
+    }
+
+    /*
+        Set default score and level, then initialize the game
+    */
+
+    newGame() {
+        // Set the initial score and level
+        game.score = 0;
+        game.level = 2;
+
+        // Show the score and draw the game board
+        $('#modal').css('visibility', 'hidden');
+        $('#score').html('Score: ' + game.score);
+        $('#game').css('filter', 'none');
+        game.drawBoard(game.level);
+    }
+
+    /*
+        Draw N x N square tiles depending on level
+    */
+
+    drawBoard(level) {
+        // Make sure the Show Hint checkbox is not checked
+        $('#showHint').prop('checked', false);
+
+        // Generate random colour based on HSL values, leaning towards bright colours
+        var h = Math.floor(Math.random() * 361);
+        var s = 80;
+        var l = 60;
+        var tileColour = 'hsl(' + h + ',' + s + '%,' + l + '%)';
+        var specialColour = 'hsl(' + h + ',' + (s + 15) + '%,' + (l + 15) + '%)';
+
+        // Generate special tile
+        var specialX = Math.floor(Math.random() * level);
+        var specialY = Math.floor(Math.random() * level);
+
+        // Generate a grid of tiles depending on the level
+        var html = '';
+        for (var i = 0; i < level; i++) {
+            html += '<div class="row">';
+
+            for (var j = 0; j < level; j++) {
+                if (i == specialY && j == specialX) {
+                    html += '<div class="tile" style="background:' + specialColour + '" data-special=1></div>';
+                } else {
+                    html += '<div class="tile" style="background:' + tileColour + '"></div>';
+                }
+            }
+
+            html += '</div>';
+        }
+
+        $('#gameBoard').html(html);
+    }
+
+    /*
+        If the clicked tile is the special tile, increase score and move on to the next level, otherwise, game over!
+    */
+
+    checkTile() {
+        if ($(this).data('special')) {
+            $('#score').html('Score: ' + ++game.score);
+            game.drawBoard(++game.level);
+        } else {
+            game.gameOver();
+        }
+    }
+
+    /*
+        Hint the player with a dashed border around the special tile
+    */
+
+    showHint() {
+        $('div[data-special=1]').css('border', $(this).prop('checked') ? '3px dashed black' : 'none');
+        $('div[data-special=1]').css('margin', $(this).prop('checked') ? '0' : '3px');
+    }
+
+    /*
+        Generate the 'Game Over' modal
+    */
+
+    gameOver() {
+        // Generate and show the 'Game Over' modal
+        var html = '';
+        html += '<div id="gameOver">';
+        html += '<h1>GAME OVER!</h1>';
+        html += '<h3>Your score is ' + game.score + '</h3>';
+        html += '<p>Submit your name to the Hall of Fame</p>';
+        html += '<form>';
+        html += '<input type="text" id="player"> <button type="submit" class="btn btn-success" id="addScore">Submit</button>';
+        html += '</form>';
+        html += '</div>';
+
+        $('#game').css('filter', 'blur(4px)');
+        $('#modal').html(html);
+        $('#modal').css('visibility', 'visible');
+    }
+
+    /*
+        Add current score and display top 10 scores
+    */
+
+    addScore() {
+        // Add current score to list of scores
+        game.player = $('#player').val();
+        scores.push({ 'player': game.player, 'score': game.score });
+
+        // Sort scores in descending order, from highest to lowest
+        scores.sort(function(a, b) {
+            return b.score - a.score;
+        });
+
+        // Generate the 'High Scores' modal
+        var html = '';
+        html += '<div id="highScores">';
+        html += '<h1>High Scores</h1>';
+        html += '<ol>';
+
+        $.each(scores, function(k, v) {
+            // Exit loop when number of scores exceed 10 
+            if (k >= 10) {
+                return false;
+            }
+
+            html += '<li><span style="margin:40px">' + v.player + '</span><span class="right">' + v.score + '</span></li>';
+        });
+
+        html += '</ol>';
+        html += '<button class="btn btn-success" id="newGame">New Game</button>';
+        html += '</div>';
+
+        $('#modal').html(html);
+    }
+}
